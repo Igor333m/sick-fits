9aafba2a9a6de9f9086863d205977499ac33846a
Igor Todorovic <igor333m@yahoo.com>
2018-11-07 08:49:33 -0800

    First styled-components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+	background: red;
+	font-size: ${props => props.huge ? '100px' : '50px'};
+`;
 
 class Page extends React.Component {
   render() {
@@ -8,6 +14,8 @@ class Page extends React.Component {
 			 <div>
 				<Meta />
 				<Header />
+				<MyButton huge>Click Me</MyButton>
+				<MyButton>Click Me</MyButton>
 				{this.props.children}
 			</div>
 		)
