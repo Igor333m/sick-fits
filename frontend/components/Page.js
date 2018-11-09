7d01c6b6814d3ff4bf2edfd20a125cb39dd91b8d
Igor Todorovic <igor333m@yahoo.com>
2018-11-08 21:12:57 -0800

    Add css theme

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -3,21 +3,38 @@ import Header from './Header';
 import Meta from './Meta';
 import styled from 'styled-components';
 
-const MyButton = styled.button`
-	background: red;
-	font-size: ${props => props.huge ? '100px' : '50px'};
+const theme = {
+	red: '#e65c00',
+	black: '#393939',
+	grey: '#3a3a3a',
+	lightgrey: '#e1e1e1',
+	offWhite: '#ededed',
+	maxWidth: '1200px',
+	bs: '0 21px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StyledPage = styled.div`
+	background: white;
+	color: black;
+`;
+
+const Inner = styled.div`
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 2rem;
+	background-color: red;
 `;
 
 class Page extends React.Component {
   render() {
 		return (
-			 <div>
+			 <StyledPage>
 				<Meta />
 				<Header />
-				<MyButton huge>Click Me</MyButton>
-				<MyButton>Click Me</MyButton>
-				{this.props.children}
-			</div>
+					<Inner>
+						{this.props.children}
+					</Inner>
+			</StyledPage>
 		)
   }
 }
